85ecba0e39339b125c86146beffea7544c872e16
Lerish-vZ <lerishavz@gmail.com>
2024-01-06 15:35:37 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import WeatherApp from "./weather/WeatherApp";
+
 class App extends React.Component {
   render(){
   return (
